51a52804993a269ca07c9984682de49f03a37429
Phil Armstead <phil.armstead@moneysupermarket.com>
2023-12-13 18:01:10 +0000

    Adding solution to days 13 (part 2, wip)

--- 2023/day-13.js
+++ 2023/day-13.js
@@ -3,93 +3,90 @@
 // Paste the following code in to your browser's dev tools and execute it
 
 {
-	const input = document.body.textContent.trim().split('\n\n')
-		.map(grid => grid.split('\n'))
+	const input = `#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
 
-	const getHorizontalLineOfSymmetry = (grid) => {
-		const values = []
-		let matches = 0
-		let match = 0
-		for (let y = 0; y < grid.length; ++y) {
-			let value = 0
-			for (let x = 0; x < grid[y].length; ++x) {
-				if (grid[y][x] === '#') {
-					value += Math.pow(2, x)
-				}
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#`
+		.trim()
+		.split('\n\n')
+		.map(grid => grid.split('\n'));
+
+	const getDifferenceCount = (line1, line2) => {
+		let differences = 0;
+		for (let i = 0; i < line1.length; ++i) {
+			if (line1[i] !== line2[i]) {
+				++differences
 			}
+		}
+		return differences;
+	};
 
-			if (y) {
-				if (value === values[y - 1 - matches * 2]) {
-					++matches
-					match = match || y
-				} else {
-					matches = 0
-					match = 0
-				}
-
-				if (value === values[y - 1 - matches * 2]) {
-					++matches
-					match = match || y
-				}
+	const getTransposedGrid = grid => {
+		const transposedArray = [];
+		for (let x = 0; x < grid[0].length; x++) {
+			transposedArray.push('');
+			for (let y = 0; y < grid.length; y++) {
+				transposedArray[x] += grid[y][x];
 			}
-
-			if (matches && matches > values.length / 2) {
-				return match
-			}
-
-			values.push(value)
 		}
 
-		return match
-	}
+		return transposedArray;
+	};
 
-	const getVerticalLineOfSymmetry = (grid) => {
-			const values = []
-			let matches = 0
-			let match = 0
+	const getLineOfSymmetry = (grid, allowedDifferences = 0) => {
+		let matches = 0;
+		let match = 0;
+		let localAllowedDifferences = allowedDifferences
+console.log(grid)
+		for (let y = 1; y < grid.length; ++y) {
+			let hadMatch = match
+			if (getDifferenceCount(grid[y], grid[y - 1 - matches * 2]) === localAllowedDifferences) {
+				localAllowedDifferences = Math.max(--localAllowedDifferences, 0)
+				++matches;
+				match = match || y;
+			} else {
+				localAllowedDifferences = allowedDifferences
+				matches = 0;
+				match = 0;
 
-			for (let x = 0; x < grid[0].length; ++x) {
-				let value = 0
-				for (let y = 0; y < grid.length; ++y) {
-					if (grid[y][x] === '#') {
-						value += Math.pow(2, y)
-					}
+				if (hadMatch && getDifferenceCount(grid[y], grid[y - 1]) === localAllowedDifferences) {
+					localAllowedDifferences = Math.max(--localAllowedDifferences, 0)
+					++matches;
+					match = y;
 				}
-				if (x) {
-					if (value === values[x - 1 - matches * 2]) {
-						++matches
-						match = match || x
-					} else {
-						matches = 0
-						match = 0
-					}
-
-					if (value === values[x - 1 - matches * 2]) {
-						++matches
-						match = match || x
-					}
-				}
-
-				if (matches && matches > values.length / 2) {
-					return match
-				}
-
-				values.push(value)
 			}
 
-		return match
-	}
+			if (matches && matches > y / 2) {
+				return match;
+			}
+		}
+
+		return match;
+	};
 
 	// Part one
-	let sumPartOne = 0
-	for (const grid of input) {
-		sumPartOne += getHorizontalLineOfSymmetry(grid) * 100
-		sumPartOne += getVerticalLineOfSymmetry(grid)
-	}
-
-	console.log('Solution to part one:', sumPartOne)
-	// 27654 < answer < 28064
+	// const sumGrid1 = (sum, grid) => sum + (getLineOfSymmetry(grid) * 100 || getLineOfSymmetry(getTransposedGrid(grid)));
+	// const sumPartOne = input.reduce(sumGrid1, 0);
+	// console.log('Solution to part one:', sumPartOne);
 
 	// Part two
-	// console.log('Solution to part two:', sumPartTwo)
+	// const sumGrid2 = (sum, grid) => sum + (getLineOfSymmetry(grid, 1) * 100 || getLineOfSymmetry(getTransposedGrid(grid), 1));
+	// const sumPartTwo = input.reduce(sumGrid2, 0);
+	// console.log('Solution to part two:', sumPartTwo);
+
+	console.log(getLineOfSymmetry(input[0], 1))
+	// console.log(getLineOfSymmetry(getTransposedGrid(input[0]), 1))
+	// console.log(getLineOfSymmetry(input[1], 1))
+	// console.log(getLineOfSymmetry(getTransposedGrid(input[1]), 1))
 }
