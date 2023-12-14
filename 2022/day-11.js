4dde7d0cd2effd982707e314d6289efc34c883f3
Phil <philarmstead@mailbox.org>
2023-12-14 20:09:38 +0000

    Adding solution to day 11, part 2 (2022)

--- 2022/day-11.js
+++ 2022/day-11.js
@@ -3,39 +3,8 @@
 // Paste the following code in to your browser's dev tools and execute it
 
 {
-	const data = `Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3
-
-Monkey 1:
-  Starting items: 54, 65, 75, 74
-  Operation: new = old + 6
-  Test: divisible by 19
-    If true: throw to monkey 2
-    If false: throw to monkey 0
-
-Monkey 2:
-  Starting items: 79, 60, 97
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 1
-    If false: throw to monkey 3
-
-Monkey 3:
-  Starting items: 74
-  Operation: new = old + 3
-  Test: divisible by 17
-    If true: throw to monkey 0
-    If false: throw to monkey 1`
-	const input = document.body.textContent
-	// const input = data
-		.trim()
-		.split('\n\n').map(scenario => {
+	const input = document.body.textContent.trim().split('\n\n').map(scenario => {
 		const lines = scenario.split('\n')
-
 		const operation = lines[2].split('old ')[1]
 		const operand = operation.substring(2)
 		return [
@@ -45,10 +14,11 @@ Monkey 3:
 		]
 	})
 
-	const processScenarios = (scenarios) => {
-		let inspectionCount = Array.from({length: scenarios.length}).fill(0)
+	const processScenarios = (scenarios, reliefFactor = null) => {
+		const inspectionCount = Array.from({ length: scenarios.length }).fill(0)
+		const rounds = reliefFactor ? 1e4 : 20
 
-		for (let i = 0; i < 20; ++i) {
+		for (let i = 0; i < rounds; ++i) {
 			for (let j = 0; j < scenarios.length; ++j) {
 				const scenario = scenarios[j]
 				while (scenario[0].length) {
@@ -62,7 +32,11 @@ Monkey 3:
 						case '+': worryLevel += operand; break
 					}
 
-					worryLevel = Math.floor(worryLevel / 3)
+					if (!reliefFactor) {
+						worryLevel = Math.floor(worryLevel / 3)
+					} else if (worryLevel >= reliefFactor) {
+						worryLevel %= reliefFactor
+					}
 
 					const target = scenario[2][Number(!!(worryLevel % scenario[2][0])) + 1]
 					scenarios[target][0].push(worryLevel)
@@ -76,9 +50,11 @@ Monkey 3:
 	}
 
 	// Part one
-	const inspectionCountPartOne = processScenarios(input)
+	const inspectionCountPartOne = processScenarios(structuredClone(input))
 	console.log('Solution to part one:', inspectionCountPartOne)
 
 	// Part two
-	console.log('Solution to part two:', 0)
+	const reliefFactor = input.reduce((acc, cur) => acc * cur[2][0], 1)
+	const inspectionCountPartTwo = processScenarios(structuredClone(input), reliefFactor)
+	console.log('Solution to part two:', inspectionCountPartTwo)
 }
