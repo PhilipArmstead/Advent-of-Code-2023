558db08df316167905f0c955af22170ecc53d9e3
Phil <philarmstead@mailbox.org>
2023-12-15 02:11:09 +0000

    Improving solution to day 4, part 2

--- 2023/day-4.js
+++ 2023/day-4.js
@@ -1,40 +1,38 @@
 // Day 4
 // Visit the input page https://adventofcode.com/2023/day/4/input
 // Paste the following code in to your browser's dev tools and execute it
-// I'm not very happy with part 2: it takes ~5 seconds to complete, which is indicative of a bad approach
 
 {
 	const input = document.body.textContent.trim()
 		.split('\n')
 		.map(line => line.substring(line.indexOf(':') + 2).split('|').map(half => half.match(/\d+/g).map(Number)))
 
-
-	// Part one
-	let sumPartOne = 0
-	input.forEach(line => {
-		sumPartOne += line[0].reduce((acc, cur) => line[1].includes(cur) ? (!acc ? 1 : acc * 2) : acc, 0)
+	const cardScores = Array.from({ length: input.length }).map(() => ({ score: 0, count: 0 }))
+	input.forEach((line, i) => {
+		cardScores[i].score = line[0].reduce((acc, cur) => {
+			if (line[1].includes(cur)) {
+				++cardScores[i].count
+				return (!acc ? 1 : acc * 2)
+			}
+			return acc
+		}
+		, 0)
 	})
 
-	console.log('Solution to part one:', sumPartOne)
+	// Part one
+	console.log('Solution to part one:', cardScores.reduce((acc, cur) => acc + cur.score, 0))
 
 
 	// Part two
-	let sumPartTwo = 0
-	const cardsToCheck = Array.from(input, (_, i) => i)
-	const cardValues = {}
-	while (cardsToCheck.length) {
-		const card = cardsToCheck.pop()
-		const line = input[card]
-		const score = cardValues[card] !== undefined
-			? cardValues[card]
-			: line[0].reduce((acc, cur) => acc + line[1].includes(cur), 0)
-
-		if (score) {
-			cardsToCheck.push(...Array.from({ length: score }, (_, i) => card + i + 1))
+	const copyCard = (i, totalCards = 0) => {
+		if (cardScores[i].count) {
+			for (let j = i + 1; j <= i + cardScores[i].count; ++j) {
+				totalCards = copyCard(j, totalCards)
+			}
 		}
 
-		++sumPartTwo
+		return ++totalCards
 	}
 
-	console.log('Solution to part two:', sumPartTwo)
+	console.log('Solution to part two:', cardScores.reduce((acc, _, i) => acc + copyCard(i), 0))
 }
