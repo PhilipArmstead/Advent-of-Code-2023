ac56353ea6bb4d2c02fcb1a2fdf09f930defd295
Phil <philarmstead@mailbox.org>
2023-12-20 15:20:27 +0000

    Refactoring Day 20 solution slightly

--- 2023/day-20.js
+++ 2023/day-20.js
@@ -5,7 +5,6 @@
 {
 	const input = document.body.textContent.trim().split('\n')
 
-	const ModuleType = { FlipFlop: 0, Conjunction: 1 }
 	const broadcasterModules = []
 	const modules = {}
 	for (const line of input) {
@@ -15,89 +14,80 @@
 			continue
 		}
 		const name = line.substring(1, line.indexOf(' '))
-		modules[name] = line[0] === '%'
-			? { type: ModuleType.FlipFlop, state: 0, targets }
-			: { type: ModuleType.Conjunction, targets, inputs: {} }
+		modules[name] = line[0] === '%' ? { isFlipFlop: true, state: 0, targets } : { inputs: {}, targets }
 	}
-	let rxInput
-	let rxInputInput = {}
+	let exitNode
+	const exitCycles = {}
 	for (const i in modules) {
 		if (modules[i].targets.includes('rx')) {
-			rxInput = i
+			exitNode = i
 		}
-		if (modules[i].type === ModuleType.Conjunction) {
+		if (!modules[i].isFlipFlop) {
 			for (const j in modules) {
-				if (modules[j].type === ModuleType.FlipFlop && modules[j].targets.includes(i)) {
+				if (modules[j].isFlipFlop && modules[j].targets.includes(i)) {
 					modules[i].inputs[j] = 0
 				}
 			}
 		}
 	}
 	for (const i in modules) {
-		if (modules[i].targets.includes(rxInput)) {
-			rxInputInput[i] = []
+		if (modules[i].targets.includes(exitNode)) {
+			exitCycles[i] = []
 		}
 	}
 
-	const broadcastSignal = (input, findHighPulseToExit = false, pulses = 0) => {
+	const pushButton = (findHighPulseToExit = false, pulses = 0) => {
 		const pulsesSent = [1, 0]
-		const signals = broadcasterModules.map(target => [target, 0, 'broadcaster'])
+		const signals = broadcasterModules.map(target => [target, 0])
 		while (signals.length) {
 			const [target, mode, source] = signals.shift()
 			const module = modules[target]
 			pulsesSent[0] += !mode
 			pulsesSent[1] += mode
 
-			if (findHighPulseToExit && !mode && rxInputInput[target] !== undefined && rxInputInput[target].length < 2) {
-				rxInputInput[target].push(pulses - (rxInputInput[target].length ? rxInputInput[target].at(-1) : 0))
+			if (!module) continue
+
+			if (findHighPulseToExit && !mode && exitCycles[target] !== undefined && exitCycles[target].length < 2) {
+				exitCycles[target].push(pulses - (exitCycles[target].length ? exitCycles[target].at(-1) : 0))
 			}
 
-			if (!module) {
-				continue
-			}
-
-			if (module.type === ModuleType.FlipFlop) {
-				if (mode) {
-					continue
-				}
+			let newMode
+			if (module.isFlipFlop) {
+				if (mode) continue
 
 				module.state = !module.state
-				module.targets.forEach(destination => signals.push([destination, module.state, target]))
-			} else if (module.type === ModuleType.Conjunction) {
+				newMode = module.state
+			} else if (!module.isFlipFlop) {
 				module.inputs[source] = mode
-				const newMode = !Object.values(module.inputs).every(mode => mode)
-				module.targets.forEach((destination) => signals.push([destination, newMode, target]))
+				newMode = !Object.values(module.inputs).every(mode => mode)
 			}
+			module.targets.forEach(destination => signals.push([destination, newMode, target]))
 		}
 
 		if (!findHighPulseToExit) {
 			return pulsesSent
 		}
 
-		const exits = Object.entries(rxInputInput)
-		if (exits.every(([, e]) =>e.length === 2)) {
-			return exits.reduce((acc, [, e]) => acc * e[1], 1)
-		}
-
-		return false
+		const exits = Object.entries(exitCycles)
+		return exits.every(([, e]) => e.length === 2)
+			? exits.reduce((acc, [, e]) => acc * e[1], 1)
+			: false
 	}
 
 	// Part one
-	const inputPartOne = structuredClone(input)
 	const sumPartOne = [0, 0]
 	for (let i = 0; i < 1000; ++i) {
-		const [low, high] = broadcastSignal(inputPartOne)
+		const [low, high] = pushButton()
 		sumPartOne[0] += low
 		sumPartOne[1] += high
 	}
 	console.log('Solution to part one:', sumPartOne[0] * sumPartOne[1])
 
 	// Part two
-	const inputPartTwo = structuredClone(input)
 	let lowestCommonDenominator = 0
 	let i = 0
-	while (!lowestCommonDenominator) {
-		lowestCommonDenominator = broadcastSignal(inputPartTwo, true, i++)
+	while (!lowestCommonDenominator && i < 10000) {
+		lowestCommonDenominator = pushButton(true, i++)
 	}
 	console.log('Solution to part two:', lowestCommonDenominator)
 }
