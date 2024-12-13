3abdc5658d310b2f0375b0458066d35848c031cb
Phil Armstead <phil.armstead@moneysupermarket.com>
2024-12-13 23:51:27 +0000

    Refactor day 12 part 2 by looking for corners

--- 2024/day-12/day-12.c
+++ 2024/day-12/day-12.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
@@ -13,13 +14,14 @@ typedef struct {
 	u16 id;
 	u16 area;
 	u16 perimeter;
+	u16 edges;
 	u8 x1;
 	u8 y1;
 	u8 x2;
 	u8 y2;
 } Plot;
 
-u16 getEdgesFromPlot(const u16 *grid, Plot *plot);
+void walkPlot(u16 *grid, u8 y, u8 x, u8 c, Plot *plot);
 
 int day12(char *filepath) {
 	struct timeval startTime;
@@ -69,8 +71,6 @@ int day12(char *filepath) {
 	const u8 startingPlotId = 100;
 	u16 plotId = startingPlotId + 1;
 
-	u8 queue[116];
-
 	for (y = 1; y < size + 1; ++y) {
 		for (u8 x = 1; x < size + 1; ++x) {
 			u16 c = grid[y * DAY_12_MAX_SIZE + x];
@@ -79,108 +79,61 @@ int day12(char *filepath) {
 				continue;
 			}
 
-			Plot plot = {plotId, 1, 0, size, size, 0, 0};
-			grid[y * DAY_12_MAX_SIZE + x] = plot.id;
+			Plot plot = { plotId, 1, 0, 0, size, size, 0, 0 };
 
-			u8 qHead = 0;
-			u8 qTail = 2;
-			queue[0] = y;
-			queue[1] = x;
-
-			while (qHead != qTail) {
-				u8 qY = queue[qHead];
-				u8 qX = queue[qHead + 1];
-
-				if (qY < plot.y1) {
-					plot.y1 = qY;
-				}
-				if (qY > plot.y2) {
-					plot.y2 = qY;
-				}
-				if (qX < plot.x1) {
-					plot.x1 = qX;
-				}
-				if (qX > plot.x2) {
-					plot.x2 = qX;
-				}
-
-				for (u8 i = 0; i < 4; ++i) {
-					i8 dY = directions[i][0];
-					i8 dX = directions[i][1];
-
-					u16 nC = grid[(qY + dY) * DAY_12_MAX_SIZE + qX + dX];
-					if (nC == c) {
-						++plot.area;
-						grid[(qY + dY) * DAY_12_MAX_SIZE + qX + dX] = plot.id;
-						queue[qTail] = qY + dY;
-						queue[qTail + 1] = qX + dX;
-						qTail += 2;
-					} else if (nC != grid[qY * DAY_12_MAX_SIZE + qX]) {
-						++plot.perimeter;
-					}
-				}
-
-				qHead += 2;
-			}
+			walkPlot(grid, y, x, c, &plot);
 
 			answerPartOne += plot.area * plot.perimeter;
-			answerPartTwo += getEdgesFromPlot(grid, &plot);
+			answerPartTwo += plot.area * plot.edges;
 
 			++plotId;
 		}
 	}
 
-	free(grid);
-
 	printChallengeSummary(12, startTime, answerPartOne, answerPartTwo);
 	return 0;
 }
 
-u16 getEdgesFromPlot(const u16 *grid, Plot *plot) {
-	u16 edges = 0;
-	bool isInGroup = false;
+void walkPlot(u16 *grid, u8 y, u8 x, u8 c, Plot *plot) {
+	grid[y * DAY_12_MAX_SIZE + x] = plot->id;
 
-	i16 steps[2] = {-1, 1};
-
-	// Horizontal edges
-	for (u8 i = 0; i < 2; ++i) {
-		for (i16 j = plot->y1; j < plot->y2 + 1; ++j) {
-			for (i16 k = plot->x1; k < plot->x2 + 1; ++k) {
-				if (grid[j * 142 + k] == plot->id && grid[(j + steps[i]) * 142 + k] != plot->id) {
-					isInGroup = true;
-				} else if (isInGroup) {
-					isInGroup = false;
-					++edges;
-				}
-			}
-
-			if (isInGroup) {
-				++edges;
-			}
-			isInGroup = false;
-		}
+	u16 up = grid[(y - 1) * DAY_12_MAX_SIZE + x];
+	u16 right = grid[y * DAY_12_MAX_SIZE + x + 1];
+	u16 down = grid[(y + 1) * DAY_12_MAX_SIZE + x];
+	u16 left = grid[y * DAY_12_MAX_SIZE + x - 1];
+	if (up != plot->id && up != c) {
+		if (right != plot->id && right != c) ++plot->edges;
+		if (left != plot->id && left != c) ++plot->edges;
+	}
+	if (down != plot->id && down != c) {
+		if (right != plot->id && right != c) ++plot->edges;
+		if (left != plot->id && left != c) ++plot->edges;
+	}
+	if (up == plot->id || up == c) {
+		u16 upLeft = grid[(y - 1) * DAY_12_MAX_SIZE + x - 1];
+		if (upLeft != plot->id && upLeft != c && (left == plot->id || left == c)) ++plot->edges;
+		u16 upRight = grid[(y - 1) * DAY_12_MAX_SIZE + x + 1];
+		if (upRight != plot->id && upRight != c && (right == plot->id || right == c)) ++plot->edges;
+	}
+	if (down == plot->id || down == c) {
+		u16 downLeft = grid[(y + 1) * DAY_12_MAX_SIZE + x - 1];
+		if (downLeft != plot->id && downLeft != c && (left == plot->id || left == c)) ++plot->edges;
+		u16 downRight = grid[(y + 1) * DAY_12_MAX_SIZE + x + 1];
+		if (downRight != plot->id && downRight != c && (right == plot->id || right == c)) ++plot->edges;
 	}
 
-	// Vertical edges
-	for (u8 i = 0; i < 2; ++i) {
-		for (i16 j = plot->x1; j < plot->x2 + 1; ++j) {
-			for (i16 k = plot->y1; k < plot->y2 + 1; ++k) {
-				if (grid[k * 142 + j] == plot->id && grid[k * 142 + j + steps[i]] != plot->id) {
-					isInGroup = true;
-				} else if (isInGroup) {
-					isInGroup = false;
-					++edges;
-				}
-			}
+	for (u8 i = 0; i < 4; ++i) {
+		i8 dY = directions[i][0];
+		i8 dX = directions[i][1];
 
-			if (isInGroup) {
-				++edges;
-			}
-			isInGroup = false;
+		u16 nC = grid[(y + dY) * DAY_12_MAX_SIZE + x + dX];
+		if (nC == c) {
+			++plot->area;
+			walkPlot(grid, y + dY, x + dX, c, plot);
+		} else if (nC != grid[y * DAY_12_MAX_SIZE + x]) {
+			++plot->perimeter;
 		}
 	}
-
-	return edges * plot->area;
 }
 
 #ifndef IS_MAIN
